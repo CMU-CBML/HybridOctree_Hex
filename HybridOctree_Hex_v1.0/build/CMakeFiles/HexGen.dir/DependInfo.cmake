35c1080a4b29ccca1c551b6cf4d76d149c33ff03
Hua Tong <huat2@andrew.cmu.edu>
2024-01-12 15:01:37 -0500

    HybridOctree_Hex_v1.0

---
+++ HybridOctree_Hex_v1.0/build/CMakeFiles/HexGen.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/14124/Downloads/HybridOctree_Hex_v1.0/HexGen.cpp" "CMakeFiles/HexGen.dir/HexGen.cpp.obj" "gcc" "CMakeFiles/HexGen.dir/HexGen.cpp.obj.d"
+  "C:/Users/14124/Downloads/HybridOctree_Hex_v1.0/Main.cpp" "CMakeFiles/HexGen.dir/Main.cpp.obj" "gcc" "CMakeFiles/HexGen.dir/Main.cpp.obj.d"
+  "C:/Users/14124/Downloads/HybridOctree_Hex_v1.0/Mesh.cpp" "CMakeFiles/HexGen.dir/Mesh.cpp.obj" "gcc" "CMakeFiles/HexGen.dir/Mesh.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
